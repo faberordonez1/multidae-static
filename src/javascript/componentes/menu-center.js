43d28bd9487ed869381dc3f6e608163ea28b0aa1
Faber Ordoñez <faberordonez1@gmail.com>
2022-04-03 19:19:48 -0500

    menu dinamico central

--- src/javascript/componentes/menu-center.js
+++ src/javascript/componentes/menu-center.js
@@ -4,35 +4,29 @@ class menuCenter extends HTMLElement{
     }
     
     connectedCallback(){
-      this.innerHTML =`<a href='index.html'>Home</a>
-        |
-        <a href="pages/login.html"
-            >Login/Register</a
-        >
-        |
-        <a
-            href="pages/inlogin.html"
-            >Toggle Security</a
-        >
-        |
-        <a
-            href="index.php?do=toggle-enforce-ssl&amp;page=/opt/lampp/htdocs/mutillidae-master/home.php"
-            >Enforce TLS</a
-        >
-        |
-        <a href="set-up-database.php"
-            >Reset DB</a
-        >
-        |
-        <a
-            href="index.php?page=show-log.php"
-            >View Log</a
-        >
-        |
-        <a
-            href="index.php?page=captured-data.php"
-            >View Captured Data</a
-        >`;
+        let menuHome = `<a href='index.html'>Home</a>|
+        <a href="pages/login.html">Login/Register</a>|
+        <a href="pages/inlogin.html">Toggle Security</a>|
+        <a href="home.html">Enforce TLS</a>|
+        <a href="set-up-database.html">Reset DB</a>|
+        <a href="show-log.html">View Log</a>|
+        <a href="index.php?page=captured-data.php">View Captured Data</a>`;
+
+        let menuOthers =  `<a href='../index.html'>Home</a>|
+        <a href="login.html">Login/Register</a>|
+        <a href="login.html">Toggle Security</a>|
+        <a href="home.html">Enforce TLS</a>|
+        <a href="set-up-database.html">Reset DB</a>|
+        <a href="show-log.html">View Log</a>|
+        <a href="captured-data.html">View Captured Data</a>`
+
+
+        var rutaAbsoluta = self.location.href;   
+		var posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
+		var rutaRelativa = rutaAbsoluta.substring( posicionUltimaBarra + "/".length , rutaAbsoluta.length );
+        
+        rutaRelativa ==='index.html'? this.innerHTML = menuHome : this.innerHTML = menuOthers;
+        
     }
 }
 
