174693f24fc53aa3ea398009d6673025535e54f1
Faber Ordoñez <faberordonez1@gmail.com>
2022-04-11 23:53:17 -0500

    se ajusta ruta menu

--- src/javascript/componentes/menu-center.js
+++ src/javascript/componentes/menu-center.js
@@ -4,29 +4,40 @@ class menuCenter extends HTMLElement{
     }
     
     connectedCallback(){
-        let menuHome = `<a href='index.html'>Home</a>|
-        <a href="pages/login.html">Login/Register</a>|
-        <a href="pages/inlogin.html">Toggle Security</a>|
-        <a href="index.html">Enforce TLS</a>|
-        <a href="pages/set-up-database.html">Reset DB</a>|
-        <a href="pages/show-log.html">View Log</a>|
-        <a href="pages/captured-data.html">View Captured Data</a>`;
+        let menuHome = `
+        <nav>
+            <ul>
+                <li><a href='index.html'>Home</a></li>
+                <li><a href="pages/login.html">Login/Register</a></li>
+                <li><a href="pages/inlogin.html">Toggle Security</a></li>
+                <li><a href="index.html">Enforce TLS</a></li>
+                <li><a href="pages/set-up-database.html">Reset DB</a></li>
+                <li><a href="pages/show-log.html">View Log</a></li>
+                <li><a href="pages/captured-data.html">View Captured Data</a></li>
+            </ul>
+        </nav>`;
 
-        let menuOthers =  `<a href='../index.html'>Home</a>|
-        <a href="login.html">Login/Register</a>|
-        <a href="login.html">Toggle Security</a>|
-        <a href="../index.html">Enforce TLS</a>|
-        <a href="set-up-database.html">Reset DB</a>|
-        <a href="show-log.html">View Log</a>|
-        <a href="captured-data.html">View Captured Data</a>`
+        let menuOthers =  `<nav>
+        <ul>
+            <li><a href='../index.html'>Home</a></li>
+            <li><a href="login.html">Login/Register</a></li>
+            <li><a href="inlogin.html">Toggle Security</a></li>
+            <li><a href="../index.html">Enforce TLS</a></li>
+            <li><a href="set-up-database.html">Reset DB</a></li>
+            <li><a href="show-log.html">View Log</a></li>
+            <li><a href="captured-data.html">View Captured Data</a></li>
+        </ul>
+    </nav>`
 
 
-        var rutaAbsoluta = self.location.href;   
-		var posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
-		var rutaRelativa = rutaAbsoluta.substring( posicionUltimaBarra + "/".length , rutaAbsoluta.length );
-        
-        rutaRelativa ==='index.html'? this.innerHTML = menuHome : this.innerHTML = menuOthers;
-        
+    var rutaAbsoluta = self.location.href;   
+    var posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
+    var rutaRelativa = rutaAbsoluta.substring( posicionUltimaBarra + "/".length , rutaAbsoluta.length );
+        if(rutaRelativa === '' || rutaRelativa ==='index.html'){
+            this.innerHTML = menuHome
+        }else{
+            this.innerHTML = menuOthers;
+        }
     }
 }
 
